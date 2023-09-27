f7df9e5c000eb9ec00c87d4161b40e2370464eb8
Swami Mahale <swamimahale1712@gmail.com>
2023-09-27 19:53:52 +0530

    work on next

--- src/components/News.js
+++ src/components/News.js
@@ -6,20 +6,22 @@ export default function News() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
-  const [pageSize] = useState(20); // pageSize is constant
+  const [totalResults, setTotalResults] = useState(0);
+  let pageSize = 5; // pageSize is constant
 
   useEffect(() => {
     async function fetchNews() {
       setLoading(true);
-      const url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=fcddded3fc954d08a4f4b585a7278780&page=${page}&pageSize=${pageSize}`;
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fcddded3fc954d08a4f4b585a7278780&page=${page}&pageSize=${pageSize}`;
       const response = await fetch(url);
       const data = await response.json();
+      setTotalResults(data.totalResults);
       setArticles(data.articles);
       setLoading(false);
     }
 
     fetchNews();
-  }, [page, pageSize]);
+  }, [page]);
 
   const handlePrev = () => {
     if (page > 1) {
@@ -27,8 +29,8 @@ export default function News() {
     }
   };
 
-  const handleNext = () => {
-    if (page * pageSize < articles.totalResults) {
+  const handleNext = async () => {
+    if (page * pageSize < totalResults) {
       setPage(page + 1);
     }
   };
@@ -50,6 +52,7 @@ export default function News() {
                   : "https://imgs.search.brave.com/UA-pyyM6Wp945k41Uv6YAUpAWJY7SVpL_1zpT278B_g/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM1/MTcwNTg2NC9waG90/by90aGUtd29yZHMt/YnJlYWtpbmctbmV3/cy1vbi1hbi1hYnN0/cmFjdC1iYWNrZ3Jv/dW5kLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1ydDVWb3Rx/LUlyM2pzVG5fb2JJ/SDdUREs1N0g5MmJ3/UzN6dWlNQnI1ZHNZ/PQ"
               }
               newsUrl={element.url}
+              dateOfPublish={element.publishedAt}
             />
           ))}
         </div>
